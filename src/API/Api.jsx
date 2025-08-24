8bade48f1709d7367c76f11c08268b24cdbfbb9b
Muhammad Umar Hassan <your_email@example.com>
2025-08-24 12:01:30 +0500

    Updated ErrorPage.jsx with new layout

--- src/API/Api.jsx
+++ src/API/Api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = "b7ecb5e3e8ae40c3858fe812ba0f4caf";
+const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 
 export const Getapi = (search) => {
   return axios.get(`https://newsapi.org/v2/everything?q=${search}&apiKey=${API_KEY}`)
